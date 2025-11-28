6dd2aa31fea2cccaf0f2701c88ff5e18c8b6e919
BHASVIC-laithtakriti24 <laith.takriti24@students.bhasvic.ac.uk>
2025-11-28 10:15:43 +0000

    prayerScript 1

---
+++ Code for the website/PrayerScript.js
@@ -0,0 +1,40 @@
+
+
+const now = new Date();
+console.log(now);
+
+let prayer1 = document.getElementById("fajr");// The variable for fajr
+let prayer2 = document.getElementById("dhuhr");//The variable for dhuhr
+let prayer3 = document.getElementById("asr");// The variable for asr
+let prayer4 = document.getElementById("maghrib");//The variable for maghrib
+let prayer5 = document.getElementById("isha");//The variable for isha
+let TimeTill = document.getElementById("timer")// sets the timer till the next prayer 
+	
+
+function TimeSet(){
+	const day = data.getDate();
+	const hour = data.getHours();
+	const minutes = data.getMinutes();
+	
+	console.log(day, hours, minutes);
+{
+
+function TimeForFajr(FajrTime){
+	prayer1;
+}
+
+function TimeForDhuhr(DhuhrTime){
+	prayer2;
+}
+
+function TimeForAsr(AsrTime){
+	prayer3;
+}
+
+function TimeForMaghrib(MaghribTime){
+	prayer4;
+}
+
+function TimeForIsha(IshaTime){	
+	prayer5; 
+}
