3718e66ff6cc5fc1b1e24161861770982822fa1a
BHASVIC-laithtakriti24 <laith.takriti24@students.bhasvic.ac.uk>
2025-11-29 22:41:31 +0000

    objects made

--- Code for the website/PrayerScript.js
+++ Code for the website/PrayerScript.js
@@ -1,4 +1,20 @@
 
+function TimeSet() {
+    let TimeTill = document.getElementById("timer"); // sets the timer till the next prayer
+    const day = data.getDate();
+    const hour = data.getHours();
+    const minutes = data.getMinutes();
+
+    console.log(day, hours, minutes);
+}
+// Object holding all the prayer times
+const prayerTimes = {
+    fajr: "5:15",
+    dhuhr: "11:50",
+    asr: "15:15",
+    maghrib: "17:30",
+    isha: "20:30"
+};
 
 // function to update the table in the HTML
 function getIDForPrayers() {
