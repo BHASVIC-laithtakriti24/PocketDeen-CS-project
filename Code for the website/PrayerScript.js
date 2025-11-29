b3dcb6e2f30cc20f0fa843ec404d98e1f67fc849
BHASVIC-laithtakriti24 <laith.takriti24@students.bhasvic.ac.uk>
2025-11-29 22:39:57 +0000

    prayer times
    I’ve managed to make the HTML table update, and now I need to implement an API to make it fully functional.

--- Code for the website/PrayerScript.js
+++ Code for the website/PrayerScript.js
@@ -1,67 +1,13 @@
 
 
-const now = new Date();
-console.log(now);
-
-
-
-function TimeSet(){
-	let TimeTill = document.getElementById("timer")// sets the timer till the next prayer 
-	const day = data.getDate();
-	const hour = data.getHours();
-	const minutes = data.getMinutes();
-	
-	console.log(day, hours, minutes);
-{
-// to get the ID
-function getIDForFajr(){
-	document.getElementById("fajr") = element.id;// The variable for fajr	
-	
+// function to update the table in the HTML
+function getIDForPrayers() {
+	// put the prayers time inside the prayers ids
+    document.getElementById("fajr").textContent = prayerTimes.fajr;
+    document.getElementById("dhuhr").textContent = prayerTimes.dhuhr;
+    document.getElementById("maghrib").textContent = prayerTimes.asr;
+    document.getElementById("asr").textContent = prayerTimes.maghrib;
+    document.getElementById("isha").textContent = prayerTimes.isha;
 }
-function getIDForDhuhr(){
-	document.getElementById("dhuhr") = element.id;//The variable for dhuhr
-}
-
-function getIDForAsr(){
-	document.getElementById("asr") = element.id;// The variable for asr
-}
-
-function getIDForMaghrib(){
-	document.getElementById("maghrib") = element.id;//The variable for maghrib
-}
-
-function getIDForIsha(){	
-	document.getElementById("isha") = element.id;//The variable for isha
-}
-
-
-// to change the ID 
-function changeIDForFajr() {
-
-	getIDForFajr();	
-	element.id = "5:45"
-	
-	concole.log(element)
-}
-
-function changeIDForDhuhr() {
-	getIDForDhuhr();	
-	
-}
-
-function changeIDForAsr() {
-	getIDForAsr();	
-	
-}
-
-function changeIDForMaghrib() {
-	getIDForMaghrib();	
-	
-}
-
-function changeIDForisha() {
-	getIDForIsha();	
-	
-}
-
-
+// calls the function so the table actually updates
+getIDForPrayers();
