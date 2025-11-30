431994b43e30a9d67206d37952004814494b455c
BHASVIC-laithtakriti24 <laith.takriti24@students.bhasvic.ac.uk>
2025-11-30 23:23:31 +0000

    Got Prayer API to work
    The prayer times started to work

--- Code for the website/PrayerScript.js
+++ Code for the website/PrayerScript.js
@@ -1,29 +1,48 @@
+ 
 
-function TimeSet() {
-    let TimeTill = document.getElementById("timer"); // sets the timer till the next prayer
-    const day = data.getDate();
-    const hour = data.getHours();
-    const minutes = data.getMinutes();
-
-    console.log(day, hours, minutes);
-}
-// Object holding all the prayer times
-const prayerTimes = {
-    fajr: "5:15",
-    dhuhr: "11:50",
-    asr: "15:15",
-    maghrib: "17:30",
-    isha: "20:30"
-};
 
 // function to update the table in the HTML
 function getIDForPrayers() {
-	// put the prayers time inside the prayers ids
     document.getElementById("fajr").textContent = prayerTimes.fajr;
     document.getElementById("dhuhr").textContent = prayerTimes.dhuhr;
-    document.getElementById("maghrib").textContent = prayerTimes.asr;
-    document.getElementById("asr").textContent = prayerTimes.maghrib;
+    document.getElementById("asr").textContent = prayerTimes.asr;      
+    document.getElementById("maghrib").textContent = prayerTimes.maghrib;
     document.getElementById("isha").textContent = prayerTimes.isha;
 }
-// calls the function so the table actually updates
-getIDForPrayers();
+
+
+// calls the function so the table updates
+fetchPrayerTimes();
+
+async function fetchPrayerTimes() {
+    try {// Sends request to API for Brighton, UK prayer timings
+        const response = await fetch("https://api.aladhan.com/v1/timingsByAddress?address=Brighton,UK");// The prayer Api
+        const data = await response.json();
+		
+		// Throws an error if the response was unsuccessful
+		if(!response.ok){
+			throw new Error("could not fetch");
+		}
+		
+		// Object holding all the prayer times
+		prayerTimes = {
+            fajr: data.data.timings.Fajr,// timings from the API
+            dhuhr: data.data.timings.Dhuhr,
+            asr: data.data.timings.Asr,
+            maghrib: data.data.timings.Maghrib,
+            isha: data.data.timings.Isha
+		}
+		// Updates the HTML table with the new prayer times
+		getIDForPrayers();
+		
+    } 
+	catch (error) { 
+		 // catches any error that occurs during the request or processing
+        console.error(error);
+    }
+}
+
+function countDownForNextPrayer(){//For the countDown
+	const now = new date();
+	
+}
